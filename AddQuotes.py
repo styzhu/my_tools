ef6fa0664bf1e37785a82925329110bbf1a764c6
StyZhu <zhu@junyuan.me>
2015-01-04 11:23:17 -0700

    add quotes tool

---
+++ AddQuotes.py
@@ -0,0 +1,28 @@
+'''
+add /" at the beginning and //n/" at the end of each line
+take argument as the source file
+Created on Jan 4, 2015
+
+@author: FunghiApe
+'''
+
+import sys
+
+
+print "File location: ", sys.argv[1]
+
+with open(sys.argv[1], "r") as f:
+    lineList = f.readlines()
+    
+    maxLineLength = 0
+    for l in lineList:
+        l = l.rstrip('\n')
+        if len(l) > maxLineLength:
+            maxLineLength = len(l)
+    
+    extraSpaceLength = 0
+    for l in lineList:
+        l = l.rstrip('\n')
+        extraSpaceLength = maxLineLength - len(l) + 4
+        extraSpace = " " * extraSpaceLength
+        print "\"{0}{1}\\n\"".format(l, extraSpace)
